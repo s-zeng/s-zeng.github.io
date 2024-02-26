7c017731d4bae3e3441235b8016227f9aa7dc743
Simon Zeng <contact@simonzeng.com>
2024-02-26 21:08:22 +0000

    Update readme

--- zelia/shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.11.tar.gz") {} }:
-
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ 
-    pandoc
-    gnumake
-    texlive.combined.scheme-full
-  ];
-}
